e086401a0868ccfcbcde313e8972137b72dca682
provideforme <evan.a.bernard@gmail.com>
2022-03-07 22:39:23 -0500

    add favicon begin favorites

--- models/profile.js
+++ models/profile.js
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose'
+import { Burger } from '../models/burger.js'
+
+
 
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
+  // favorites: [{type: Schema.Types.ObjectId, ref: 'Burger'}],
 }, {
   timestamps: true
 })
+
 
 const Profile = mongoose.model('Profile', profileSchema)
 
